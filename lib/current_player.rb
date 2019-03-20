30efca166e02e7634a0c271a23793e3a2263918a
Joshua Gelberger <joshua_gelberger@alumni.brown.edu>
2019-03-20 01:04:15 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,4 +1,14 @@
+def position_taken?(board, location)
+  board[location] != " " && board[location] != ""
+end
+
 def turn_count(board) 
   counter = 0
   board.each do |space|
-    if p
+    if position_taken?(board, space)
+      counter += 1 
+    end
+  end
+  
+  return counter
+end
